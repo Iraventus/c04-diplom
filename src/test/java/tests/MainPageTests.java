0c16c29dc4507074bc63f576a458def9ca4c23f6
Nick Tokarev <tokarev_ns>
2021-03-15 22:35:30 +0300

    edit TO

--- src/test/java/tests/MainPageTests.java
+++ src/test/java/tests/MainPageTests.java
@@ -1,6 +1,7 @@
 package tests;
 
 import config.ConfigHelper;
+import io.qameta.allure.Feature;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +10,7 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-
+@Feature("Main page tests")
 public class MainPageTests extends TestBase {
 
     @Test
--- src/test/java/tests/NewsPage.java
+++ src/test/java/tests/NewsPage.java
@@ -1,5 +1,7 @@
 package tests;
 
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,6 +14,7 @@ import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+@Feature("News page tests")
 public class NewsPage extends TestBase {
     private final String search = "//*[@placeholder='Поиск по новостям']";
 
@@ -34,6 +37,7 @@ public class NewsPage extends TestBase {
     }
 
     @Test
+    @Story("Console test")
     @DisplayName("Console log should not have any errors")
     void consoleLogShouldNotHaveErrors() {
         open("/news");
